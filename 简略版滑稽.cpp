b5de1cbef086d4d6a1954b15777826104588291f
Leopold <31884198+POD153@users.noreply.github.com>
2017-09-12 22:31:43 +0800

    Add files via upload

---
+++ 简略版滑稽.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (void)
+{    cout<<"          ooo"<<'\n';
+     cout<<"      oo      oo"<<'\n'; 
+     cout<<"     o          o"<<'\n';
+     cout<<"   oo            oo"<<'\n';
+     cout<<"  oo [  0]  [  0] oo"<<'\n';
+     cout<<" o                  o"<<'\n';
+     cout<<"  o        I       o"<<'\n';
+     cout<<"  oo              oo"<<'\n';
+     cout<<"   oo      V     oo"<<'\n';
+     cout<<"     o          o"<<'\n';
+     cout<<"      oo      oo"<<'\n';
+     cout<<"        o    o"<<'\n';
+ } 
